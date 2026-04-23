1b82d18635b725344617a31d207c28dd43f0af2a
radioincamix <mariopaucar1977@gmail.com>
2026-04-23 15:46:11 -0500

    Add files via upload

---
+++ imsearch.php
@@ -0,0 +1,115 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html lang="es-ES" dir="ltr">
+	<head>
+		<title>Buscar - Radio Cooperativa 90.3 FM</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="author" content="Edited By MajeSa" />
+		<meta name="generator" content="Incomedia WebSite X5 Professional 10.1.6.49 - www.websitex5.com" />
+		<meta name="viewport" content="width=1080" />
+		<link rel="icon" href="favicon.png" type="image/png" />
+		<link rel="stylesheet" type="text/css" href="style/reset.css" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/print.css" media="print" />
+		<link rel="stylesheet" type="text/css" href="style/style.css" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/template.css" media="screen" />
+		<link rel="stylesheet" type="text/css" href="style/menu.css" media="screen" />
+		<!--[if lte IE 7]><link rel="stylesheet" type="text/css" href="style/ie.css" media="screen" /><![endif]-->
+		
+		<script type="text/javascript" src="res/jquery.js?49"></script>
+		<script type="text/javascript" src="res/x5engine.js?49"></script>
+		<script type="text/javascript">
+			x5engine.boot.push('x5engine.gallery(imTemplateSlideShow_1_settings)');x5engine.boot.push('x5engine.gallery(imTemplateSlideShow_2_settings)');x5engine.boot.push('x5engine.gallery(imTemplateSlideShow_3_settings)');x5engine.boot.push('x5engine.gallery(imTemplateSlideShow_4_settings)');x5engine.boot.push('x5engine.gallery(imTemplateSlideShow_5_settings)');x5engine.boot.push('x5engine.gallery(imTemplateSlideShow_6_settings)');x5engine.boot.push('x5engine.gallery(imTemplateSlideShow_7_settings)');x5engine.boot.push('x5engine.gallery(imTemplateSlideShow_8_settings)');
+		</script>
+		
+	</head>
+	<body>
+		<div id="imHeaderBg"></div>
+		<div id="imFooterBg"></div>
+		<div id="imPage">
+			<div id="imHeader">
+				<h1 class="imHidden">Buscar - Radio Cooperativa 90.3 FM</h1>
+				
+				<div id="imSlideshowContent_1" style="position: absolute; top: -1px; left: 1px; width: 1080px; height: 382px;"><div id="imFlashContainer_1"></div></div><script type="text/javascript">var imTemplateSlideShow_1_settings = {'target': '#imSlideshowContent_1', 'width': 1080, 'height': 382, 'disableTouch': true, 'loadingImage': 'res/imLoad.gif', 'autoplay': true, 'random': false, 'thumbsPosition': 'none', 'showButtons': false, 'backgroundColor': 'transparent', 'media': [{'width': 1080,'height': 382, 'type': 'image','url': 'gallery/ba8.png','autoplayTime': 8000,'effect': 'none'},{'width': 1080,'height': 382, 'type': 'image','url': 'gallery/ba7.png','autoplayTime': 8000,'effect': 'fade'},{'width': 1080,'height': 382, 'type': 'image','url': 'gallery/ba12.png','autoplayTime': 5000,'effect': 'verticalSlices'},{'width': 1080,'height': 382, 'type': 'image','url': 'gallery/ba11.png','autoplayTime': 8000,'effect': 'none'},{'width': 1080,'height': 382, 'type': 'image','url': 'gallery/ba14.png','autoplayTime': 8000,'effect': 'none'},{'width': 1080,'height': 382, 'type': 'image','url': 'gallery/ba15.png','autoplayTime': 8000,'effect': 'none'},{'width': 1080,'height': 382, 'type': 'image','url': 'gallery/ba16.png','autoplayTime': 5000,'effect': 'none'},{'width': 1080,'height': 382, 'type': 'image','url': 'gallery/BANECOP-2.png','autoplayTime': 5000,'effect': 'none'}]}; </script>
+				<div id="imSlideshowContent_2" style="position: absolute; top: 10px; left: 6px; width: 403px; height: 91px;"><div id="imFlashContainer_2"></div></div><script type="text/javascript">var imTemplateSlideShow_2_settings = {'target': '#imSlideshowContent_2', 'width': 403, 'height': 91, 'disableTouch': true, 'loadingImage': 'res/imLoad.gif', 'autoplay': true, 'random': false, 'thumbsPosition': 'none', 'showButtons': false, 'backgroundColor': 'transparent', 'media': [{'width': 403,'height': 91, 'type': 'image','url': 'gallery/Radio-Cooperativa.png','autoplayTime': 2000,'effect': 'verticalSlices'},{'width': 403,'height': 91, 'type': 'image','url': 'gallery/Radio-Cooperativa.png','autoplayTime': 2000,'effect': 'scramble'},{'width': 403,'height': 91, 'type': 'image','url': 'gallery/Radio-Cooperativa.png','autoplayTime': 2000,'effect': 'squares'},{'width': 403,'height': 91, 'type': 'image','url': 'gallery/Radio-Cooperativa.png','autoplayTime': 2000,'effect': 'rotateZoom'}]}; </script>
+				<div id="imSlideshowContent_3" style="position: absolute; top: -3px; left: 880px; width: 198px; height: 105px;"><div id="imFlashContainer_3"></div></div><script type="text/javascript">var imTemplateSlideShow_3_settings = {'target': '#imSlideshowContent_3', 'width': 198, 'height': 105, 'disableTouch': true, 'loadingImage': 'res/imLoad.gif', 'autoplay': true, 'random': false, 'thumbsPosition': 'none', 'showButtons': false, 'backgroundColor': 'transparent', 'media': [{'width': 198,'height': 105, 'type': 'image','url': 'gallery/3RNYN35.png','autoplayTime': 5000,'effect': 'none'}]}; </script>
+				<div id="imSlideshowContent_4" style="position: absolute; top: 206px; left: 771px; width: 303px; height: 110px;"><div id="imFlashContainer_4"></div></div><script type="text/javascript">var imTemplateSlideShow_4_settings = {'target': '#imSlideshowContent_4', 'width': 303, 'height': 110, 'disableTouch': true, 'loadingImage': 'res/imLoad.gif', 'autoplay': true, 'random': false, 'thumbsPosition': 'none', 'showButtons': false, 'backgroundColor': 'transparent', 'media': [{'width': 303,'height': 110, 'type': 'image','url': 'gallery/rADIO-VIVO.png','autoplayTime': 5000,'effect': 'none'}]}; </script>
+				<div id="imSlideshowContent_5" style="position: absolute; top: 352px; left: 220px; width: 855px; height: 32px;"><div id="imFlashContainer_5"></div></div><script type="text/javascript">var imTemplateSlideShow_5_settings = {'target': '#imSlideshowContent_5', 'width': 855, 'height': 32, 'disableTouch': true, 'loadingImage': 'res/imLoad.gif', 'autoplay': true, 'random': false, 'thumbsPosition': 'none', 'showButtons': false, 'backgroundColor': 'transparent', 'media': [{'width': 855,'height': 32, 'type': 'image','url': 'gallery/link-1.png','autoplayTime': 10000,'effect': 'verticalSlices'},{'width': 855,'height': 32, 'type': 'image','url': 'gallery/link-2.png','autoplayTime': 6000,'effect': 'squares'},{'width': 855,'height': 32, 'type': 'image','url': 'gallery/link-3.png','autoplayTime': 8000,'effect': 'carousel'}]}; </script>
+				<div id="imSlideshowContent_6" style="position: absolute; top: 348px; left: 56px; width: 146px; height: 28px;"><div id="imFlashContainer_6"></div></div><script type="text/javascript">var imTemplateSlideShow_6_settings = {'target': '#imSlideshowContent_6', 'width': 146, 'height': 28, 'disableTouch': true, 'loadingImage': 'res/imLoad.gif', 'autoplay': true, 'random': false, 'thumbsPosition': 'none', 'showButtons': false, 'backgroundColor': 'transparent', 'media': [{'width': 146,'height': 28, 'type': 'image','url': 'gallery/2.png','autoplayTime': 5000,'effect': 'none'}]}; </script>
+				<div id="imSlideshowContent_7" style="position: absolute; top: 339px; left: 6px; width: 42px; height: 40px;"><div id="imFlashContainer_7"></div></div><script type="text/javascript">var imTemplateSlideShow_7_settings = {'target': '#imSlideshowContent_7', 'width': 42, 'height': 40, 'disableTouch': true, 'loadingImage': 'res/imLoad.gif', 'autoplay': true, 'random': false, 'thumbsPosition': 'none', 'showButtons': false, 'backgroundColor': 'transparent', 'media': [{'width': 42,'height': 40, 'type': 'image','url': 'gallery/whasapp-2.png','autoplayTime': 5000,'effect': 'none'}]}; </script>
+				<div style="position: absolute; top: 213px; left: 969px; width: 100px; height: 100px; overflow: hidden;"><!-- BEGINS: AUTO-GENERATED MUSES RADIO PLAYER CODE -->
+<script type="text/javascript" src="https://hosted.muses.org/mrp.js"></script>
+<script type="text/javascript">
+MRP.insert({
+'url':'http://freeuk6.listen2myradio.com:4912/;',
+'codec':'mp3',
+'volume':100,
+'autoplay':false,
+'jsevents':true,
+'buffering':0,
+'title':'Radio Cooperativa 90.3 FM',
+'wmode':'transparent',
+'skin':'abrahadabra',
+'width':100,
+'height':141
+});
+</script>
+<!-- ENDS: AUTO-GENERATED MUSES RADIO PLAYER CODE --></div>
+				<div id="imSlideshowContent_8" style="position: absolute; top: 5px; left: 541px; width: 332px; height: 20px;"><div id="imFlashContainer_8"></div></div><script type="text/javascript">var imTemplateSlideShow_8_settings = {'target': '#imSlideshowContent_8', 'width': 332, 'height': 20, 'disableTouch': true, 'loadingImage': 'res/imLoad.gif', 'autoplay': true, 'random': false, 'thumbsPosition': 'none', 'showButtons': false, 'backgroundColor': 'transparent', 'media': [{'width': 332,'height': 20, 'type': 'image','url': 'gallery/anos.png','autoplayTime': 5000,'effect': 'none'}]}; </script>
+			</div>
+			<a class="imHidden" href="#imGoToCont" title="Salta el menu principal">Vaya al Contenido</a>
+			<a id="imGoToMenu"></a><p class="imHidden">Menu Principal:</p>
+			<div id="imMnMn" class="auto">
+				<ul class="auto">
+					<li id="imMnMnNode0">
+						<a href="index.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span>Página de inicio</span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode3">
+						<a href="quienes-somos.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span>Quienes Somos</span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode4">
+						<a href="programacion.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span>Programacion</span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode5">
+						<a href="contactenos.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span>Contactenos</span>
+							</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div id="imContentGraphics"></div>
+			<div id="imContent">
+				<a id="imGoToCont"></a>
+				<h2 id="imPgTitle">Resultados de búsqueda</h2><?php
+$search = new imSearch();
+$keys = isset($_GET['search']) ? $_GET['search'] : "";
+$page = isset($_GET['page']) ? $_GET['page'] : 0;
+$type = isset($_GET['type']) ? $_GET['type'] : "pages"; ?>
+<div class="searchPageContainer">
+<?php echo $search->search($keys, $page, $type); ?>
+</div>
+				  
+				<div class="imClear"></div>
+			</div>
+			<div id="imFooter">
+				
+				<div id="imSlideshowContent_9" style="position: absolute; top: 11px; left: 5px; width: 149px; height: 37px;"><div id="imFlashContainer_9"></div></div><script type="text/javascript">var imTemplateSlideShow_9_settings = {'target': '#imSlideshowContent_9', 'width': 149, 'height': 37, 'autoplay': true, 'random': false, 'thumbsPosition': 'none', 'disableTouch': true, 'loadingImage': 'res/imLoad.gif', 'showButtons': false, 'backgroundColor': 'transparent', 'media': [{'width': 149,'height': 37,'type': 'image','url': 'gallery/Radio-Cooperativa_zf1d5p65.png','autoplayTime': 5000,'effect': 'none'}]}; x5engine.boot.push('x5engine.gallery(imTemplateSlideShow_9_settings)');</script>
+				<div id="imSlideshowContent_10" style="position: absolute; top: 6px; left: 950px; width: 124px; height: 52px;"><div id="imFlashContainer_10"></div></div><script type="text/javascript">var imTemplateSlideShow_10_settings = {'target': '#imSlideshowContent_10', 'width': 124, 'height': 52, 'autoplay': true, 'random': false, 'thumbsPosition': 'none', 'disableTouch': true, 'loadingImage': 'res/imLoad.gif', 'showButtons': false, 'backgroundColor': 'transparent', 'media': [{'width': 124,'height': 52,'type': 'image','url': 'gallery/grupo-famasat.jpg','autoplayTime': 5000,'effect': 'none'}]}; x5engine.boot.push('x5engine.gallery(imTemplateSlideShow_10_settings)');</script>
+			</div>
+		</div>
+		<span class="imHidden"><a href="#imGoToCont" title="Lea esta página de nuevo">Regreso al contenido</a> | <a href="#imGoToMenu" title="Lea este sitio de nuevo">Regreso al menu principal</a></span>
+		
+	<noscript class="imNoScript"><div class="alert alert-red">Para utilizar este sitio tienes que habilitar JavaScript</div></noscript>
+				</body>
+</html>
